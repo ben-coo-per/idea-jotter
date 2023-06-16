ddeb0b9a9250b3f1e55804c3db7675e24cf10eb6
Ben Cooper <cooperben83@gmail.com>
2023-06-15 21:55:02 -0400

    Initial Commit

---
+++ Idea Box/Idea_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Idea_BoxApp.swift
+//  Idea Box
+//
+//  Created by Ben Cooper on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Idea_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
