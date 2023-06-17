4bcd9055d27ca877d39a3db6217336859685b29b
Ben Cooper <cooperben83@gmail.com>
2023-06-16 22:54:04 -0400

    added composable architecture start

--- Idea Box/ContentView.swift
+++ Idea Box/ContentView.swift
@@ -4,23 +4,56 @@
 //
 //  Created by Ben Cooper on 6/15/23.
 //
-
+import ComposableArchitecture
 import SwiftUI
 
 struct ContentView: View {
+    let store: StoreOf<IdeaList>
+    
+    @State private var isCreatingIdea: Bool = false
+    @State private var description: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            NavigationView {
+                VStack{
+                    List(viewStore.ideas, id: \.self.id) { idea in
+                        Text(idea.description).swipeActions {
+                            Button("Archived") {
+                                print("Awesome!")
+                            }
+                            .tint(.purple)
+                        }
+                    }
+                    Group {
+                        if isCreatingIdea {
+                            HStack {
+                                TextField("Enter your idea", text: $description)
+                                Button(action: {
+                                    viewStore.send(.addIdea(self.description))
+                                    self.description = ""
+                                    self.isCreatingIdea = false
+                                }) {
+                                    Text("Create")
+                                }
+                            }
+                        } else{
+                            Button("New Idea", action: {isCreatingIdea.toggle()})
+                        }
+                    }.padding()
+                }.navigationTitle("Ideas")
+                // TODO: full screen view of an idea text w/ back button
+                // TODO: archive page -> swipe to archive & unarchive
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store(initialState: IdeaList.State()) {
+                IdeaList()
+            }
+        )
     }
 }
--- Idea Box/Idea_BoxApp.swift
+++ Idea Box/Idea_BoxApp.swift
@@ -4,14 +4,49 @@
 //
 //  Created by Ben Cooper on 6/15/23.
 //
-
+import ComposableArchitecture
 import SwiftUI
+
+struct Idea: Equatable {
+    var description: String
+    var id: UUID
+    var date: Date = Date()
+}
+
+struct IdeaList: ReducerProtocol {
+    public struct State: Equatable {
+        var ideas: [Idea] = [
+            Idea(description: "Fooby dooby", id: UUID()),
+            Idea(description: "Fooby dooby 2: Electric shoe", id: UUID())
+        ]
+    }
+    enum Action: Equatable {
+        case addIdea(String)
+        case removeIdea(Int)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+            case let .addIdea(idea):
+                state.ideas.append(Idea(description: idea, id: UUID()))
+                return .none
+        case let .removeIdea(index):
+            state.ideas.remove(at: index)
+            return .none
+            }
+    }
+}
+
 
 @main
 struct Idea_BoxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(initialState: IdeaList.State()) {
+                    IdeaList()
+                }
+            )
         }
     }
 }
