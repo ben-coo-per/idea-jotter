29280bde81a812c431fe3c362d089d29ab6c867d
Ben Cooper <cooperben83@gmail.com>
2023-06-17 19:53:59 -0400

    pulled addIdea input out into own view

--- Idea Box/ContentView.swift
+++ Idea Box/ContentView.swift
@@ -8,10 +8,32 @@ import ComposableArchitecture
 import SwiftUI
 
 struct ContentView: View {
+    let store: StoreOf<IdeaList>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            NavigationView {
+                VStack{
+                    List(viewStore.ideas, id: \.self.id) { idea in
+                        Text(idea.description)
+                    }
+                    AddIdeaInputBoxView(store: viewStore)
+                }.navigationTitle("Ideas")
+                // TODO: full screen view of an idea text w/ back button
+                // TODO: archive page -> swipe to archive & unarchive
+            }
+        }
+    }
+}
+
+
+
+
+struct AddIdeaInputBoxView: View {
     private enum Field {
         case description
     }
-    let store: StoreOf<IdeaList>
+    @State public var store: ViewStore<IdeaList.State, IdeaList.Action>
     
     @State private var isCreatingIdea: Bool = false
     @State private var description: String = ""
@@ -27,52 +49,38 @@ struct ContentView: View {
     }
     
     var body: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            NavigationView {
-                VStack{
-                    List(viewStore.ideas, id: \.self.id) { idea in
-                        Text(idea.description).swipeActions {
-                            Button("Archive") {
-                                print("Awesome!")
-                            }
-                            .tint(.purple)
+        Group {
+            if isCreatingIdea {
+                VStack {
+                    HStack {
+                        Text("Enter your idea")
+                        Spacer()
+                        Button(action: {
+                            store.send(.addIdea(self.description))
+                            description = ""
+                            isCreatingIdea = false
+                        }) {
+                            Text("Create")
                         }
-                    }
-                    Group {
-                        if isCreatingIdea {
-                            VStack {
-                                HStack {
-                                    Text("Enter your idea").foregroundStyle(.secondary)
-                                    Spacer()
-                                    Button(action: {
-                                        viewStore.send(.addIdea(self.description))
-                                        description = ""
-                                        isCreatingIdea = false
-                                    }) {
-                                        Text("Create")
-                                    }
-                                }.frame(maxWidth: .infinity)
-                                TextEditor(text: $description)
-                                    .padding(.horizontal)
-                                    .focused($focusField, equals: .description)
-                            }.onAppear {
-                                focusField = .description
-                            }
-                        } else{
-                            Button("Add a new idea", action: {isCreatingIdea.toggle()})
-                        }
-                    }
-                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                        .onEnded({ value in ideaboxSwipe(gestureVal: value.translation.height)
-                        }))
-                    .padding()
-                }.navigationTitle("Ideas")
-                // TODO: full screen view of an idea text w/ back button
-                // TODO: archive page -> swipe to archive & unarchive
+                    }.frame(maxWidth: .infinity)
+                    TextEditor(text: $description)
+                        .focused($focusField, equals: .description)
+                }.onAppear {
+                    focusField = .description
+                }
+            } else{
+                Button("Add a new idea", action: {isCreatingIdea.toggle()})
             }
         }
+        .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+            .onEnded({ value in ideaboxSwipe(gestureVal: value.translation.height)
+            }))
+        .padding()
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
